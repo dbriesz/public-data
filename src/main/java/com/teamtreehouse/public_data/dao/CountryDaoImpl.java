5c3f3b02b702be9e31a404b5102fa41f1fa6245c
David Riesz <dbriesz@users.noreply.github.com>
2017-02-03 17:11:36 -0800

    Commented code in CountryDaoImpl

--- src/main/java/com/teamtreehouse/public_data/dao/CountryDaoImpl.java
+++ src/main/java/com/teamtreehouse/public_data/dao/CountryDaoImpl.java
@@ -102,6 +102,8 @@ public class CountryDaoImpl implements CountryDao {
 
     @Override
     public Country findByCode(List<Country> countries, String code) {
+
+        // Uses a stream to find a country by code
         return countries.stream()
                 .filter(country -> country.getCode().equals(code))
                 .findFirst()
@@ -110,6 +112,8 @@ public class CountryDaoImpl implements CountryDao {
 
     @Override
     public Country minInternetUsers() {
+
+        // Uses a stream to compare each country and find the country with the lowest percentage of internet users
         return countries.stream()
                 .filter(country -> country.getInternetUsers() != null)
                 .min(Comparator.comparingDouble(Country::getInternetUsers))
@@ -118,6 +122,8 @@ public class CountryDaoImpl implements CountryDao {
 
     @Override
     public Country maxInternetUsers() {
+
+        // Uses a stream to compare each country and find the country with the highest percentage of internet users
         return countries.stream()
                 .filter(country -> country.getInternetUsers() != null)
                 .max(Comparator.comparingDouble(Country::getInternetUsers))
@@ -126,6 +132,8 @@ public class CountryDaoImpl implements CountryDao {
 
     @Override
     public Country minAdultLiteracy() {
+
+        // Uses a stream to compare each country and find the country with the lowest adult literacy rate
         return countries.stream()
                 .filter(country -> country.getAdultLiteracyRate() != null)
                 .min(Comparator.comparingDouble(Country::getAdultLiteracyRate))
@@ -134,6 +142,8 @@ public class CountryDaoImpl implements CountryDao {
 
     @Override
     public Country maxAdultLiteracy() {
+
+        // Uses a stream to compare each country and find the country with the highest adult literacy rate
         return countries.stream()
                 .filter(country -> country.getAdultLiteracyRate() != null)
                 .max(Comparator.comparingDouble(Country::getAdultLiteracyRate))
@@ -145,12 +155,16 @@ public class CountryDaoImpl implements CountryDao {
         Double avg;
         Double sum = 0.00;
         int count = 0;
+
+        // Iterates through the countries and gets a total internet user percentage for all countries
         for (Country country : countries) {
             if (country.getInternetUsers() != null) {
                 sum += country.getInternetUsers();
                 count++;
             }
         }
+
+        // Calculates the average percentage of internet users for all countries
         avg = sum / count;
         return avg;
     }
@@ -160,12 +174,16 @@ public class CountryDaoImpl implements CountryDao {
         Double avg;
         Double sum = 0.00;
         int count = 0;
+
+        // Iterates through the countries and gets a total adult literacy rate for all countries
         for (Country country : countries) {
             if (country.getAdultLiteracyRate() != null) {
                 sum += country.getAdultLiteracyRate();
                 count++;
             }
         }
+
+        // Calculates the average adult literacy rate for all countries
         avg = sum / count;
         return avg;
     }
@@ -186,32 +204,55 @@ public class CountryDaoImpl implements CountryDao {
         Double sumInternetSquared = 0.00;
         Double sumLiteracySquared = 0.00;
 
+        // Iterates through all countries in order to calculate the corelation coefficient between internet users and adult literacy rate
         for (Country country : countries) {
+
+            // If a country has a null value for internet users or adult literacy rate, that country is excluded from the calculation
             if (country.getInternetUsers() != null && country.getAdultLiteracyRate() != null) {
+
+                // Stores internet users and adult literacy rate for the current country
                 internetUsers = country.getInternetUsers();
                 adultLiteracy = country.getAdultLiteracyRate();
 
+                // Calculates internet users * adult literacy rate for each country
                 multiplied = internetUsers * adultLiteracy;
+
+                // Calculates internet users squared for each country
                 internetSquared = internetUsers * internetUsers;
+
+                // Calculates adult literacy rate squared for each country
                 literacySquared = adultLiteracy * adultLiteracy;
 
+                // Iterates the sum of internet user percentages
                 sumInternetUsers += internetUsers;
+
+                // Iterates the sum of adult literacy rates
                 sumAdultLiteracy += adultLiteracy;
+
+                // Iterates the sum of internet user percentage * adult literacy rate
                 sumMultiplied += multiplied;
+
+                // Iterates the sum of internet user percentages squared
                 sumInternetSquared += internetSquared;
+
+                // Iterates the sum of adult literacy rates squared
                 sumLiteracySquared += literacySquared;
                 n++;
             }
         }
 
+        // Breaks the correlation coefficient equation into separate parts
         Double covalence = (n * sumMultiplied - (sumInternetUsers * sumAdultLiteracy));
         Double sigmax = sumInternetSquared - (sumInternetUsers * sumInternetUsers);
         Double sigmay = sumLiteracySquared - (sumAdultLiteracy * sumAdultLiteracy);
+
+        // Final calculation of the correlation coefficient equation
         correlationCoefficient =  covalence / Math.sqrt(sigmax * sigmay);
 
         return correlationCoefficient;
     }
 
+    // Returns a value of true if the correlation coefficient is positive
     @Override
     public boolean isPositive() {
         return getCorrelationCoefficient() >= 0;
